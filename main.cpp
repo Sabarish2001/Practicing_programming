4c6774ca84b5a687fe80a1d691f3cff812b92a2c
Sabarish.B <76895003+Sabarish2001@users.noreply.github.com>
2022-06-28 19:13:47 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,70 @@
 #include<iostream>
-#include<fstream>
 
 using namespace std;
 
+class Shape
+{
+public:
+
+    virtual int area()=0;
+    virtual int perimeter()=0;
+};
+
+class Rectangle : public Shape
+{
+public:
+    int length;
+    int breadth;
+
+    Rectangle(int len,int bread){length=len;breadth=bread;}
+
+    int area(){return length*breadth;}
+    int perimeter(){return (2*(length+breadth));}
+
+    friend ostream & operator<<(ostream &out,Rectangle &r);
+};
+
+ostream & operator<<(ostream &out,Rectangle &r)
+{
+    out<<"Length = "<<r.length<<"Breadth = "<<r.breadth;
+    return out;
+}
+
+class Circle : public Shape
+{
+public:
+    int radius;
+
+    Circle(int rad){radius=rad;}
+
+    int area(){return (3.145*radius*radius);}
+    int perimeter(){return (2*3.145*radius);}
+
+    friend ostream & operator<<(ostream &out,Circle &c);
+};
+
+ostream & operator<<(ostream &out,Circle &c)
+{
+    out<<"Radius = "<<c.radius;
+    return out;
+}
+
 int main()
 {
-    ofstream MyFile;
-    MyFile.open(example.txt);
-    MyFile<<"Hello this is my first file"<<endl;
-    MyFile.close();
+    Shape *s;
+    Rectangle r(10,5);
+    s=&r;
+    cout<<"area of Rectangle = "<<s->area()<<endl;
+    cout<<"perimeter of Rectangle = "<<s->perimeter()<<endl;
+    cout<<r<<endl;
+
+    Circle c(10);
+    s=&c;
+    cout<<"Area of Circle = "<<s->area()<<endl;
+    cout<<"Perimeter of Circle = "<<s->perimeter()<<endl;
+    cout<<c;
+    return 0;
+
+
 }
+
